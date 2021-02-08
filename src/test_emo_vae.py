5be7c8f9848e4ec5e695ad8176c2d72c44cb4fc9
eric <eric.guizzo@hotmail.it>
2021-02-08 15:37:19 +0100

    testing emo vae

--- src/test_emo_vae.py
+++ src/test_emo_vae.py
@@ -6,7 +6,7 @@ from models import emo_vae
 def loss_f(input, recon, truth, emo_preds, beta):
     input = input.repeat(1,4,1,1)
     recon_loss = F.binary_cross_entropy(recon, input)
-    emo_loss = beta * F.binary_cross_entropy(truth, emo_preds.detach())
+    emo_loss = beta * F.binary_cross_entropy(emo_preds, truth)
 
     return recon_loss + emo_loss
 
@@ -14,7 +14,7 @@ batch_size = 1
 beta = 0.2
 label = torch.rand(batch_size,3,requires_grad=True)
 
-x = torch.rand(batch_size,1,512,128)
+x = torch.rand(batch_size,1,512,128)  #dummy batch, same size as spectrograms
 model = emo_vae()
 
 outs, preds = model(x)
