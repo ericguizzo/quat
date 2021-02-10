59a8296114e9b5d92d3863a903293402ed1bddad
eric <eric.guizzo@hotmail.it>
2021-02-10 11:57:42 +0100

    fixing training_autoencoder

---
+++ src/loss_emo.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from models import emo_vae
+
+def emo_loss(input, recon, truth, emo_preds, beta):
+    input = input.repeat(1,4,1,1)
+    recon_loss = F.binary_cross_entropy(recon, input)
+    valence_loss = F.binary_cross_entropy(emo_preds[:,0],truth[:,0])
+    arousal_loss = F.binary_cross_entropy(emo_preds[:,1],truth[:,1])
+    dominance_loss = F.binary_cross_entropy(emo_preds[:,2],truth[:,2])
+
+    emo_loss = beta * (valence_loss + arousal_loss + dominance_loss)
+    total_loss = recon_loss + emo_loss
+
+    return {'total':total_loss, 'emo':emo_loss, 'valence':valence_loss,
+            'arousal':arousal_loss, 'dominance':dominance_loss}
