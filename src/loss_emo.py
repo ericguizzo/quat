62b937f7059b6371a41126f958116b91bb60e348
eric <eric.guizzo@hotmail.it>
2021-02-16 15:17:55 +0100

    starting experiments with emo_ae_vgg

--- src/loss_emo.py
+++ src/loss_emo.py
@@ -5,9 +5,9 @@ import numpy as np
 def emo_loss(input, recon, truth, emo_preds, beta):
     input = input.repeat(1,4,1,1)
     recon_loss = F.binary_cross_entropy(recon, input)
-    valence_loss = F.MSELoss(emo_preds[:,0],truth[:,0])
-    arousal_loss = F.MSELoss(emo_preds[:,1],truth[:,1])
-    dominance_loss = F.MSELoss(emo_preds[:,2],truth[:,2])
+    valence_loss = F.mse_oss(emo_preds[:,0],truth[:,0])
+    arousal_loss = F.mse_oss(emo_preds[:,1],truth[:,1])
+    dominance_loss = F.mse_oss(emo_preds[:,2],truth[:,2])
 
     emo_loss = beta * (valence_loss + arousal_loss + dominance_loss)
     total_loss = recon_loss + emo_loss
