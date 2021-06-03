1a5c0e070968c1dffea032f8ac8b60e5237bc9ab
ericguizzo <eric.guizzo@hotmail.it>
2021-06-03 16:53:44 +0200

    fa

--- src/loss_emo.py
+++ src/loss_emo.py
@@ -9,10 +9,8 @@ def emo_loss(input, recon, truth, v, a, d, beta):
     #split activation (sum quat channels)
 
     recon = torch.unsqueeze(torch.sum(recon, axis=1), dim=1) / 4.
-    print ('CAZZOOOOOO', input.shape, recon.shape)
     recon_loss = F.binary_cross_entropy(recon, input)
-    #print ('cazzo', v, truth.shape)
-    '''
+
     valence_loss = F.mse_loss(v, truth[:,0])
     arousal_loss = F.mse_loss(a, truth[:,1])
     dominance_loss = F.mse_loss(d, truth[:,2])
@@ -22,8 +20,8 @@ def emo_loss(input, recon, truth, v, a, d, beta):
 
     return {'total':total_loss, 'recon': recon_loss.detach().item(), 'emo':emo_loss.detach().item(),
         'valence':valence_loss.detach().item(),'arousal':arousal_loss.detach().item(), 'dominance':dominance_loss.detach().item()}
-    '''
-    return {'total':recon_loss}
+
+    #return {'total':recon_loss}
 
 def simple_loss(input, recon, truth, v, a, d, beta):
     #just for testing. simplest reconstruction loss
