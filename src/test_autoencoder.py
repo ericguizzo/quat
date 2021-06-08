5ead1f3d60c040c2e62169facf4338fbc364d13e
ericguizzo <eric.guizzo@hotmail.it>
2021-06-08 17:02:53 +0200

    fw

--- src/test_autoencoder.py
+++ src/test_autoencoder.py
@@ -1,12 +1,16 @@
 import torch
 from torch import nn
 import numpy as np
-from models import r2he
+from models import r2he, simple_autoencoder
 from torchsummary import summary
 
 
-x = torch.rand(1,1,512, 128)
-
+x = torch.rand(4,1,512, 128)
+model = simple_autoencoder()
+print ('input_dim', x.shape)
+x=model(x)
+print ('output_dim', x.shape)
+'''
 model = r2he(verbose=True,
              latent_dim=100,
              quat=True,
@@ -17,3 +21,4 @@ print (model)
 print ('TESTING DIMENSIONS')
 print ('input_dim', x.shape)
 x,v,a,d=model(x)
+'''
