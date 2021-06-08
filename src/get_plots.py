244e8c9091f223ad5701f91b22f21abbef685a79
ericguizzo <eric.guizzo@hotmail.it>
2021-06-08 15:31:27 +0200

    fw

--- src/get_plots.py
+++ src/get_plots.py
@@ -36,7 +36,8 @@ parser.add_argument('--sample_rate', type=int, default=16000)
 #'../new_experiments/experiment_6_beta0_noquat.txt/models/model_xval_iemocap_exp6_beta0_noquat.txt_run1_fold0'
 #'../new_experiments/experiment_4_beta0.txt/models/model_xval_iemocap_exp4_beta0.txt_run1_fold0'
 #'../new_experiments/experiment_3_beta0.txt/models/model_xval_iemocap_exp3_beta0.txt_run1_fold0'
-parser.add_argument('--model_path', type=str, default='../new_experiments/experiment_6_beta0_noquat.txt/models/model_xval_iemocap_exp6_beta0_noquat.txt_run1_fold0')
+'../new_experiments/experiment_7_beta0.txt/models/model_xval_iemocap_exp7_beta0.txt_run1_fold0'
+parser.add_argument('--model_path', type=str, default='../new_experiments/experiment_7_beta0.txt/models/model_xval_iemocap_exp7_beta0.txt_run1_fold0')
 parser.add_argument('--model_name', type=str, default='r2he')
 parser.add_argument('--model_quat', type=str, default='False')
 parser.add_argument('--model_in_channels', type=int, default=1)
