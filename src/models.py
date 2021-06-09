854677a610b1daf24196924d37f63e91a04f3574
ericguizzo <eric.guizzo@hotmail.it>
2021-06-09 12:09:57 +0200

    fw

--- src/models.py
+++ src/models.py
@@ -245,11 +245,11 @@ class r2he(nn.Module):
         #    print('output a: ', arousal.shape)
         #    print('output d: ', dominance.shape)
 
-        
+
         x = torch.tensor(x, requires_grad=True)
         return x
 
-
+'''
 
 class simple_autoencoder(nn.Module):
     def __init__(self,
@@ -288,6 +288,40 @@ class simple_autoencoder(nn.Module):
         x = torch.sigmoid(x.view(-1, 1, self.time_dim, 128))
 
         #dummy = torch.tensor([0])
+        return x
+'''
+class simple_autoencoder(nn.Module):
+    def __init__(self):
+        super(simple_autoencoder, self).__init__()
+        ## encoder layers ##
+        # conv layer (depth from 3 --> 16), 3x3 kernels
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        # conv layer (depth from 16 --> 4), 3x3 kernels
+        self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+        # pooling layer to reduce x-y dims by two; kernel and stride of 2
+        self.pool = nn.MaxPool2d(2, 2)
+
+        ## decoder layers ##
+        ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
+        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(16, 3, 2, stride=2)
+
+    def forward(self, x):
+        ## encode ##
+        # add hidden layers with relu activation function
+        # and maxpooling after
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        # add second hidden layer
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)  # compressed representation
+
+        ## decode ##
+        # add transpose conv layers, with relu activation function
+        x = F.relu(self.t_conv1(x))
+        # output layer (with sigmoid for scaling from 0 to 1)
+        x = F.sigmoid(self.t_conv2(x))
+
         return x
 '''
 
