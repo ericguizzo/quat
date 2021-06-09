c2eb02e261d2661aa83c7334805b7ca0b3172f41
ericguizzo <eric.guizzo@hotmail.it>
2021-06-09 17:02:35 +0200

    fw

--- src/models.py
+++ src/models.py
@@ -295,7 +295,7 @@ class simple_autoencoder(nn.Module):
 #"VGG16": [64,64,"M",128,128,"M",256,256,256,"M",512,512,512,"M",512,512,512,"M",],
 
 class simple_autoencoder(nn.Module):
-    def __init__(self, hidden_size=100 ,flatten_dim=32768):
+    def __init__(self, hidden_size=2000 ,flatten_dim=32768):
         super(simple_autoencoder, self).__init__()
         ## encoder layers ##
         # conv layer (depth from 3 --> 16), 3x3 kernels
