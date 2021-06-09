fb268540f8a3392775ad071d2d33da21ea4a55d7
ericguizzo <eric.guizzo@hotmail.it>
2021-06-09 12:17:42 +0200

    fw

--- src/training_autoencoder_simple.py
+++ src/training_autoencoder_simple.py
@@ -177,7 +177,7 @@ for epoch in range(args.num_epochs):
             optimizer.step()
 
             #loss = loss.detach().cpu().item()
-            train_batch_losses.append({'total':loss.detach().cpu().item(), 'emo':0, 'recon':0,
+            train_batch_losses.append({'total':loss.cpu().numpy(), 'emo':0, 'recon':0,
                           'valence':0, 'arousal':0, 'dominance':0})
             pbar.update(1)
             #del loss
