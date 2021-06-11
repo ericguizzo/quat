99a57e312a298fd363d70c996f7c7a10f633bbda
ericguizzo <eric.guizzo@hotmail.it>
2021-06-11 12:40:06 +0200

    wfd

--- src/models.py
+++ src/models.py
@@ -303,16 +303,18 @@ class simple_autoencoder(nn.Module):
         self.hidden_size = hidden_size
         self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
-        self.conv3 = nn.Conv2d(32, 4, 3, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
 
         self.pool = nn.MaxPool2d(2, 2)
         #self.hidden = nn.Linear(flatten_dim, hidden_size*4)
         #self.decoder_input = nn.Linear(hidden_size*4, flatten_dim)
         ## decoder layers ##
         ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
-        self.t_conv1 = nn.ConvTranspose2d(4, 32, 2, stride=2)
-        self.t_conv2 = nn.ConvTranspose2d(32, 16, 2, stride=2)
-        self.t_conv3 = nn.ConvTranspose2d(16, 1, 2, stride=2)
+        self.t_conv1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(64, 32, 2, stride=2)
+        self.t_conv3 = nn.ConvTranspose2d(32, 16, 2, stride=2)
+        self.t_conv4 = nn.ConvTranspose2d(16, 1, 2, stride=2)
 
 
 
@@ -326,7 +328,10 @@ class simple_autoencoder(nn.Module):
         x = self.pool(x)
         x = F.relu(self.conv3(x))
         x = self.pool(x)
-        #print ('cazzo', x.shape)
+        x = F.relu(self.conv4(x))
+        x = self.pool(x)
+
+        print ('cazzo', x.shape)
         #hidden dim
         #x = torch.flatten(x, start_dim=1)
         #print (x.shape)
@@ -338,9 +343,10 @@ class simple_autoencoder(nn.Module):
         ## decode ##
         x = F.relu(self.t_conv1(x))
         x = F.relu(self.t_conv2(x))
-        x = self.t_conv3(x)
+        x = F.relu(self.t_conv3(x))
+        x = F.sigmoid(self.t_conv4(x))
 
-        return F.sigmoid(x)
+        return x
 '''
 class simple_autoencoder(nn.Module):
     def __init__(self):
