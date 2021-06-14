e2ad5ab816b6ca30577ff352d0c38f8d0f68a8a1
ericguizzo <eric.guizzo@hotmail.it>
2021-06-14 13:06:52 +0200

    vwerg

--- src/exp_instance.py
+++ src/exp_instance.py
@@ -222,7 +222,7 @@ if __name__ == '__main__':
     parser.add_argument('--ids', type=str, default='[9]')
     parser.add_argument('--first', type=int, default=1)
     parser.add_argument('--last', type=int, default=1)
-    parser.add_argument('--gpu_id', type=int, default=1)
+    parser.add_argument('--gpu_id', type=int, default=0)
     args = parser.parse_args()
 
     grid_search(experiments_folder=args.experiments_folder,
