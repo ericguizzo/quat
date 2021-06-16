c0b7079dfa8c67cda713e9fdda274f88eb2e571e
ericguizzo <eric.guizzo@hotmail.it>
2021-06-16 16:59:18 +0200

    faevsg

--- src/loss_emo.py
+++ src/loss_emo.py
@@ -11,6 +11,7 @@ def emo_loss(input, recon, truth, pred, beta):
     #recon = torch.unsqueeze(torch.sum(recon, axis=1), dim=1) / 4.
     #recon = recon[:,0,:,:]
     #recon = torch.unsqueeze(torch.sum(recon, axis=1), dim=1) / 4.
+
     recon = torch.unsqueeze(torch.sqrt(torch.sum(recon**2, axis=1)), dim=1)
 
     recon_loss = F.binary_cross_entropy(input.squeeze(), recon.squeeze())
