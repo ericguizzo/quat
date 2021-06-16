06cde048f6259de94d1c9419f1befb2d9d2daed7
ericguizzo <eric.guizzo@hotmail.it>
2021-06-16 16:57:54 +0200

    faevsg

--- src/exp_instance.py
+++ src/exp_instance.py
@@ -219,10 +219,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--experiments_folder', type=str, default='experiments_quat')
     parser.add_argument('--output_folder', type=str, default='../new_experiments')
-    parser.add_argument('--ids', type=str, default='[10]')
+    parser.add_argument('--ids', type=str, default='[13]')
     parser.add_argument('--first', type=int, default=1)
     parser.add_argument('--last', type=int, default=1)
-    parser.add_argument('--gpu_id', type=int, default=1)
+    parser.add_argument('--gpu_id', type=int, default=0)
     args = parser.parse_args()
 
     grid_search(experiments_folder=args.experiments_folder,
