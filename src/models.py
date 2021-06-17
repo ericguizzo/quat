1cc814fd412122f8c1bcf015ad9b81ee8259f4e7
ericguizzo <eric.guizzo@hotmail.it>
2021-06-17 15:44:20 +0200

    fegre

--- src/models.py
+++ src/models.py
@@ -295,7 +295,7 @@ class simple_autoencoder(nn.Module):
 #"VGG16": [64,64,"M",128,128,"M",256,256,256,"M",512,512,512,"M",512,512,512,"M",],
 
 class simple_autoencoder(nn.Module):
-    def __init__(self, quat=True, hidden_size=2048 ,flatten_dim=16384,
+    def __init__(self, quat=True, hidden_size=2048 ,flatten_dim=131072,
                  classifier_dropout=0.5, num_classes=5):
         super(simple_autoencoder, self).__init__()
         ## encoder layers ##
@@ -370,13 +370,13 @@ class simple_autoencoder(nn.Module):
         x = F.relu(self.conv2(x))
         x = self.pool(x)
         #x = self.conv2_bn(x)
-
+        '''
         x = F.relu(self.conv3(x))
         x = self.pool(x)
 
         x = F.relu(self.conv4(x))
         x = self.pool(x)
-
+        '''
         '''
         x = F.sigmoid(self.conv5(x))
         x = self.pool(x)
@@ -394,11 +394,11 @@ class simple_autoencoder(nn.Module):
     def decode(self, x):
         #x = F.relu(self.decoder_input(x))
 
-        x = x.view(-1, 64, 32, 8)
+        x = x.view(-1, 32, 128, 32)
         #x1 = F.relu(self.t_conv0(x1))
         #x = F.relu(self.t_conv1(x))
-        x = F.relu(self.t_conv2(x))
-        x = F.relu(self.t_conv3(x))
+        #x = F.relu(self.t_conv2(x))
+        #x = F.relu(self.t_conv3(x))
 
         x = F.relu(self.t_conv4(x))
         #x = self.tconv2_bn(x)
