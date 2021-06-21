0e8fe1644054b7e8bdf2b0bf2f39c442e0e98e86
ericguizzo <eric.guizzo@hotmail.it>
2021-06-21 12:33:28 +0200

    fgre

--- src/exp_instance.py
+++ src/exp_instance.py
@@ -219,9 +219,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--experiments_folder', type=str, default='experiments_quat')
     parser.add_argument('--output_folder', type=str, default='../new_experiments')
-    parser.add_argument('--ids', type=str, default='[16]')
+    parser.add_argument('--ids', type=str, default='[17]')
     parser.add_argument('--first', type=int, default=1)
-    parser.add_argument('--last', type=int, default=10)
+    parser.add_argument('--last', type=int, default=1)
     parser.add_argument('--gpu_id', type=int, default=0)
     args = parser.parse_args()
 
