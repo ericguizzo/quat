d21fcbf07abe9d0a0f75755f8d1de6fb090060c2
ericguizzo <eric.guizzo@hotmail.it>
2021-06-23 15:37:42 +0200

    fwgr3

--- src/test_autoencoder.py
+++ src/test_autoencoder.py
@@ -1,16 +1,16 @@
 import torch
 from torch import nn
 import numpy as np
-from models import simple_autoencoder, r2he, VGG
+from models import simple_autoencoder, r2he, VGGNet
 from torchsummary import summary
 
 
-x = torch.rand(5, 1, 64, 64)
-model = VGG(quat=False, flatten_dim=2048)
-x = model(x)
-'''
-model = r2he(quat=True, classifier_quat=True, batch_normalization=False)
-#model = simple_autoencoder(quat=True)
+x = torch.rand(5, 1, 512, 128)
+#model = VGG(quat=False, flatten_dim=2048)
+#x = model(x)
+
+#model = r2he(quat=True, classifier_quat=True, batch_normalization=False)
+model = simple_autoencoder(quat=True)
 print (model)
 
 #model = simple_autoencoder(quat=True)
@@ -21,7 +21,7 @@ y, pred = model(x)
 print ('output_dim', y.shape)
 l = model.get_embeddings(x)
 print ('embeddings: ', l.shape)
-'''
+
 
 #compute number of parameters
 model_params = sum([np.prod(p.size()) for p in model.parameters()])
