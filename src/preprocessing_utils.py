22695b65874f93d719d0ad4008eed47d85def6dd
ericguizzo <eric.guizzo@hotmail.it>
2021-07-08 16:12:10 +0200

    wfergwe

--- src/preprocessing_utils.py
+++ src/preprocessing_utils.py
@@ -255,6 +255,7 @@ def preprocess_foldable_item(sounds_list, max_file_length, get_label_function, p
             #print (sound_file)
             samples, sr = librosa.core.load(sound_file, sr=librosa_SR)  #read audio
             print (len(samples))
+            print (np.max(samples))
             if np.max(samples) > 0.05:  #if sound is not empty
                 if NORMALIZATION:
                     samples = np.divide(samples, np.max(samples))
