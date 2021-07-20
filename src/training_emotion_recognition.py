a4db28f7e05686afd473318d9a91a7072f303a34
ericguizzo <eric.guizzo@hotmail.it>
2021-07-20 12:50:15 +0200

    cvergwfed

--- src/training_emotion_recognition.py
+++ src/training_emotion_recognition.py
@@ -125,11 +125,11 @@ if args.model_name == 'VGGNet':
                    quat=args.model_quat,
                    num_classes=args.model_num_classes
                    )
-if args.model_name == 'AlexNet':
+elif args.model_name == 'AlexNet':
     model = AlexNet(quat=args.model_quat,
                     num_classes=args.model_num_classes
                     )
-elif:
+else:
     raise ValueError('Invalid model name')
 
 model = model.to(device)
