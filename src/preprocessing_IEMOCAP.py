c2e2f0cd115f09d90ccc4db35f0dad5078b67505
ericguizzo <eric.guizzo@hotmail.it>
2021-07-28 17:27:45 +0200

    cvfegrw

--- src/preprocessing_IEMOCAP.py
+++ src/preprocessing_IEMOCAP.py
@@ -35,7 +35,7 @@ print ('Features type: ' + str(FEATURES_TYPE))
 #and change num_classes_IEMOCAP
 num_classes_IEMOCAP = 5
 
-
+'''[
 label_to_int = {'neu':0,
                 'ang':1,
                 'hap':2,
@@ -59,7 +59,7 @@ label_to_int = {'neu':0,
                 'dis':4,
                 'oth':4,
                 'xxx':4}
-'''
+
 
 '''
 label_to_int = {'neu':0,
