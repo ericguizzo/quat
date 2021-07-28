eabb09dd2710c4d116181b1180b5eff646b3574f
ericguizzo <eric.guizzo@hotmail.it>
2021-07-28 15:37:14 +0200

    cvfegrw

--- src/loss_emo.py
+++ src/loss_emo.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 
-
 def emo_loss(recon, sounds, truth, pred, beta):
     #split activation (sum quat channels)
 
@@ -16,7 +15,7 @@ def emo_loss(recon, sounds, truth, pred, beta):
 
     #recon_loss = F.binary_cross_entropy_with_logits(recon, sounds.squeeze())
 
-    recon_loss = F.binary_cross_entropy(recon, sounds.squeeze())
+    recon_loss = F.binary_cross_entropy(recon.squeeze(), sounds.squeeze())
     #recon_loss = F.mse_loss(recon, sounds.squeeze())
 
     #valence_loss = F.mse_loss(v[:,0].squeeze(), truth[:,0].squeeze())
