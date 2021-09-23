74f4b33d45fbf6bd824fd1f5a202f95d77f88eb1
ericguizzo <eric.guizzo@hotmail.it>
2021-09-23 16:52:15 +0200

    cvegrwef

--- src/loss_emo.py
+++ src/loss_emo.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def _to_one_hot(y, num_classes):
     scatter_dim = len(y.size())
+    y = torch.tensor(y, dtype=torch.int64)
     y_tensor = y.view(*y.size(), -1)
     zeros = torch.zeros(*y.size(), num_classes, dtype=torch.int64)
 
