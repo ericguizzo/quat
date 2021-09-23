e40d9fa37ccb1f7e3d5955e0a7f06bd1048fefc3
ericguizzo <eric.guizzo@hotmail.it>
2021-09-23 16:21:36 +0200

    cvegrwef

--- src/loss_emo.py
+++ src/loss_emo.py
@@ -52,6 +52,7 @@ def emo_loss_vad(recon, sounds, truth, pred, beta, beta_vad, at_term=0):
     recon_loss = F.binary_cross_entropy(recon.squeeze(), sounds.squeeze())
     c_p, v_p, a_p, d_p = pred
 
+    print ('cccaaaaaaCVERGIWJFEOVIWFNVS', torch.argmax(c_p, axis=1), truth[:,0].long())
     valence_loss = F.binary_cross_entropy(v_p.squeeze(), truth[:,1].squeeze())
     arousal_loss = F.binary_cross_entropy(a_p.squeeze(), truth[:,2].squeeze())
     dominance_loss = F.binary_cross_entropy(d_p.squeeze(), truth[:,3].squeeze())
