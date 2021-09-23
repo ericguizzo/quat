b341225f8100031139cdfd3478a93c8a23a96cc6
ericguizzo <eric.guizzo@hotmail.it>
2021-09-23 18:11:39 +0200

    cvegrwef

--- src/loss_emo.py
+++ src/loss_emo.py
@@ -72,9 +72,9 @@ def emo_loss_vad(recon, sounds, truth, pred, beta, beta_vad, at_term=0):
     total_loss = recon_loss + emo_loss + at_term
 
     acc = torch.sum(torch.argmax(c_p, axis=1) == truth[:,0]) / c_p.shape[0]
-    acc_valence = torch.sum(v_p == truth[:,1]) / v_p.shape[0]
-    acc_arousal = torch.sum(a_p == truth[:,2]) / a_p.shape[0]
-    acc_dominance = torch.sum(d_p == truth[:,3]) / d_p.shape[0]
+    acc_valence = torch.sum(torch.round(v_p) == truth[:,1]) / v_p.shape[0]
+    acc_arousal = torch.sum(torch.round(a_p) == truth[:,2]) / a_p.shape[0]
+    acc_dominance = torch.sum(torch.round(d_p) == truth[:,3]) / d_p.shape[0]
 
     print ("CVFEGRWEF", torch.sum(v_p == truth[:,1]), v_p, truth[:,1], v_p.shape[0])
     if isinstance(at_term, int):
