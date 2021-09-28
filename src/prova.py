68bb39a0d4c64f6b8f0599e9b04e968c63943ab8
ericguizzo <eric.guizzo@hotmail.it>
2021-09-28 12:44:04 +0200

    cdsvfegrw

--- src/prova.py
+++ src/prova.py
@@ -3,5 +3,5 @@ from torch import nn
 import torch.nn.functional as F
 import os
 
-a = "/Users/eric/Downloads/EMOVO/f1/neu-f1-b3.wav"
-print (os.path.basename(a).split('-')[0])
+a = "/Users/eric/Desktop/sapienza/quat/savee/JE/a02.wav"
+print (os.path.basename(a).split('.')[0][:-2])
