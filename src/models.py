47b2555069d6b40b80a9a8f5402463c7b89a679e
ericguizzo <eric.guizzo@hotmail.it>
2021-10-06 15:07:05 +0200

    cdsvfegrw

--- src/models.py
+++ src/models.py
@@ -558,6 +558,7 @@ class simple_autoencoder_2_vad_mod(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
         self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv5 = nn.Conv2d(128, 4, 3, padding=1)
 
         self.conv1_bn = nn.BatchNorm2d(2)
         self.conv2_bn = nn.BatchNorm2d(4)
@@ -569,10 +570,11 @@ class simple_autoencoder_2_vad_mod(nn.Module):
         ## decoder layers ##
         ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
         if quat:
-            self.t_conv1 = QuaternionTransposeConv(128, 64, kernel_size=3, stride=[2,1], padding=1, output_padding=[1,0])
-            self.t_conv2 = QuaternionTransposeConv(64, 32, kernel_size=3, stride=[2,1], padding=1, output_padding=[1,0])
-            self.t_conv3 = QuaternionTransposeConv(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1)
-            self.t_conv4 = QuaternionTransposeConv(16, 4, kernel_size=3, stride=1, padding=1, output_padding=0)
+            self.t_conv1 = QuaternionTransposeConv(4, 128, kernel_size=3, stride=[2,1], padding=1, output_padding=[1,0])
+            self.t_conv2 = QuaternionTransposeConv(128, 64, kernel_size=3, stride=[2,1], padding=1, output_padding=[1,0])
+            self.t_conv3 = QuaternionTransposeConv(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
+            self.t_conv4 = QuaternionTransposeConv(32, 16, kernel_size=3, stride=1, padding=1, output_padding=0)
+            self.t_conv5 = QuaternionTransposeConv(16, 4, kernel_size=3, stride=1, padding=1, output_padding=0)
 
             self.tconv1_bn = QuaternionBatchNorm2d(4)
             self.tconv2_bn = QuaternionBatchNorm2d(4)
