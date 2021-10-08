2d8139b90cb05dee6a16a2ac5a1e6886a007bee8
ericguizzo <eric.guizzo@hotmail.it>
2021-10-08 16:31:43 +0200

    cdsvfegrw

--- src/exp_instance.py
+++ src/exp_instance.py
@@ -222,9 +222,9 @@ if __name__ == '__main__':
     parser.add_argument('--experiments_folder', type=str, default='experiments_quat')
     parser.add_argument('--output_folder', type=str, default='../new_experiments')
     parser.add_argument('--ids', type=str, default='[323]')
-    parser.add_argument('--first', type=int, default=14)
-    parser.add_argument('--last', type=int, default=14)
-    parser.add_argument('--gpu_id', type=int, default=0)
+    parser.add_argument('--first', type=int, default=15)
+    parser.add_argument('--last', type=int, default=15)
+    parser.add_argument('--gpu_id', type=int, default=1)
     args = parser.parse_args()
 
 
