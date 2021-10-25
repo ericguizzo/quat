baad02688c4987606e16c1bc663bc2a0ce8f2368
ericguizzo <eric.guizzo@hotmail.it>
2021-10-25 15:45:04 +0200

    dfgrewe

--- src/exp_instance.py
+++ src/exp_instance.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
     parser.add_argument('--ids', type=str, default='[511]')
     parser.add_argument('--first', type=int, default=1)
     parser.add_argument('--last', type=int, default=6)
-    parser.add_argument('--gpu_id', type=int, default=1)
+    parser.add_argument('--gpu_id', type=int, default=0)
     args = parser.parse_args()
 
 
