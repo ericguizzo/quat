03fda5bbfb2cea3551d8ab65dbe5287464c59ee3
ericguizzo <eric.guizzo@hotmail.it>
2022-11-18 20:04:58 +0100

    sbatch

--- src/sbatch_job.sh
+++ src/sbatch_job.sh
@@ -1,18 +1,3 @@
-#! /bin/bash
-
-#SBATCH --job-name="training"
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=8
-#SBATCH --output=PROVAjob%J.out
-#SBATCH --error=PROVAjob%J.err
-#SBATCH --partition=normal
-#SBATCH --gres=gpu:0
-
-module load cuda/9.2
-
-python3 dummy_job.py
-
-
 #!/bin/bash
 #SBATCH -D /users/aczk407/test/quat/src  # Working directory
 #SBATCH --job-name=test_1                 # Job name
