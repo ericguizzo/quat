7405a434a6ed93e5da90bfed2885799a700d6e47
ericguizzo <eric.guizzo@hotmail.it>
2022-11-21 13:56:10 +0100

    revision experiments

--- src/sbatch_job.sh
+++ src/sbatch_job.sh
@@ -4,8 +4,8 @@
 #SBATCH --mail-type=END,FAIL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=eric.guizzo@city.ac.uk         # Where to send mail	
 #SBATCH --exclusive                          # Exclusive use of nodes
-#SBATCH --nodes=2                            # Run on 2 nodes (each node has 48 cores)
-#SBATCH --ntasks-per-node=48                 # Use all the cores on each node
+#SBATCH --nodes=1                            # Run on 2 nodes (each node has 48 cores)
+#SBATCH --ntasks-per-node=8                 # Use all the cores on each node
 #SBATCH --mem=0                              # Expected memory usage (0 means use all available memory)
 #SBATCH --time=72:00:00                      # Time limit hrs:min:sec
 #SBATCH --output=experiment_job_%j.out        # Standard output and error log [%j is replaced with the jobid]
