de8dace93b42e3eeac55f05be77b2e5d53274079
ericguizzo <eric.guizzo@hotmail.it>
2022-11-24 11:32:37 +0100

    revision experiments

--- src/sbatch_job_tess_red.sh
+++ src/sbatch_job_tess_red.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-type=END,FAIL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=eric.guizzo@city.ac.uk         # Where to send mail	
 #SBATCH --nodes=1                            # Run on 2 nodes (each node has 48 cores)
-#SBATCH --ntasks-per-node=8                 # Use all the cores on each node
+#SBATCH --ntasks-per-node=6                 # Use all the cores on each node
 #SBATCH --mem=60G                              # Expected memory usage (0 means use all available memory)
 #SBATCH --time=72:00:00                      # Time limit hrs:min:sec
 #SBATCH --output=experiment_job_%j.out        # Standard output and error log [%j is replaced with the jobid]
