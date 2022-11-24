8b9f8ccfc12041c983c04dd00a20ce032438eea8
ericguizzo <eric.guizzo@hotmail.it>
2022-11-24 11:48:52 +0100

    revision experiments

--- src/sbatch_job.sh
+++ src/sbatch_job.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #SBATCH -D /users/aczk407/archive/aczk407/eric_phd/sapienza/quat/src  # Working directory
-#SBATCH --job-name=exp_quat                 # Job name
+#SBATCH --job-name=quat_1                 # Job name
 #SBATCH --mail-type=END,FAIL                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=eric.guizzo@city.ac.uk         # Where to send mail	
 #SBATCH --nodes=1                            # Run on 2 nodes (each node has 48 cores)
 #SBATCH --ntasks-per-node=6                 # Use all the cores on each node
 #SBATCH --mem=60G                              # Expected memory usage (0 means use all available memory)
 #SBATCH --time=72:00:00                      # Time limit hrs:min:sec
-#SBATCH --output=experiment_job_%j.out        # Standard output and error log [%j is replaced with the jobid]
-#SBATCH --error=experiment_job_%j.error
+#SBATCH --output=experiment_quat_1_%j.out        # Standard output and error log [%j is replaced with the jobid]
+#SBATCH --error=experiment_quat_1_%j.error
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gengpu
 
