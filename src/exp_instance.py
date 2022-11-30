9874832e2c8a7116f7e00888994cc867c104a0b8
ericguizzo <eric.guizzo@hotmail.it>
2022-11-30 14:07:43 +0100

    revision experiments

--- src/exp_instance.py
+++ src/exp_instance.py
@@ -224,7 +224,7 @@ if __name__ == '__main__':
     parser.add_argument('--ids', type=str, default='[811, 812, 814]')
     parser.add_argument('--first', type=int, default=1)
     parser.add_argument('--last', type=int, default=12)
-    parser.add_argument('--gpu_id', type=int, default=0)
+    parser.add_argument('--gpu_id', type=int, default=1)
     args = parser.parse_args()
 
 
